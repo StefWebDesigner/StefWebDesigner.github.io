c19de891e59e6c95017428fac527ae8454313e3e
StefWebDesigner <67237726+StefWebDesigner@users.noreply.github.com>
2021-08-11 14:31:34 -0400

    Rough draft 1.2 => added animated button/dropdown menus -> dropdown menus not entirely working as planned

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,41 @@
-// //==========================================================================
+
+
+//  ===========DROP DOWN MENU SECTIONS ==============
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.button--dropdown')) {
+    var dropdowns = document.getElementsByClassName("button--dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // // GLOBAL VARIABLES
 // const alertBanner = document.querySelector("#alert");
